7da7c4b566a7f3537bdd49f9ba3ce48a37f77e34
pedrovrima <pedrovrima@gmail.com>
2020-05-25 11:59:57 -0300

    added test to new functionalities

--- validate-date-test/test.js
+++ validate-date-test/test.js
@@ -9,5 +9,11 @@ test('is valid date', function (t) {
     t.equal(validateDate('02/29/2000', boolean=false), 'Valid Date');
     t.equal(validateDate('02/29/2000', boolean=true), true);
     t.equal(validateDate('02/29/2001',boolena=true), false);
+    t.equal(validateDate('02/27/2001',boolean=true,format="mm/dd/yyyy"),true);
+    t.equal(validateDate('27/02/2001',boolean=true,format="dd/mm/yyyy"),true);
+    t.equal(validateDate('27/02/2001',boolean=true,format="mm/dd/yyyy"),false);
+    t.equal(validateDate('27/2001/02',boolean=true,format="dd/yyyy/mm"),true);
+    t.equal(validateDate('27/02/2001',boolean=true,format="mm/mm/yyyy"),false);
+
     t.end()
 });
