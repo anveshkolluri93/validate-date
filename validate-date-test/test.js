ec30f86f4f22393898bd22eee35aab917059b9e1
pedrovrima <pedrovrima@gmail.com>
2020-05-25 15:45:16 -0300

    fixed typo

--- validate-date-test/test.js
+++ validate-date-test/test.js
@@ -8,7 +8,7 @@ test('is valid date', function (t) {
     t.equal(validateDate('02/2/9/2001', boolean=false), 'Invalid Format');
     t.equal(validateDate('02/29/2000', boolean=false), 'Valid Date');
     t.equal(validateDate('02/29/2000', boolean=true), true);
-    t.equal(validateDate('02/29/2001',boolena=true), false);
+    t.equal(validateDate('02/29/2001',boolean=true), false);
     t.equal(validateDate('02/27/2001',boolean=true,format="mm/dd/yyyy"),true);
     t.equal(validateDate('27/02/2001',boolean=true,format="dd/mm/yyyy"),true);
     t.equal(validateDate('27/02/2001',boolean=true,format="mm/dd/yyyy"),false);
